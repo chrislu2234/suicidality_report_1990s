6dd4045ca6d06f25aab7ea3195d25d56180e6dda
chrislu2234 <chrislu2234@gmail.com>
2023-04-22 18:28:02 -0400

    Almost everything except for simulated and data tests

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -10,17 +10,46 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Clean data ####
-# [...UPDATE THIS...]
-raw_data <- read_csv("inputs/data/raw_data.csv")
+# Suicide Rates Data are already cleaned. Refer to Data Section of Paper and Readme for source of data and method of reproduction.
+# Open CSV and save into suicides_data
+suicides_data <- read_csv("inputs/data/compiled_compressed_mortality_1973_to_2006.csv")
 
+# Decapitalize all names
+names(suicides_data) <- tolower(names(suicides_data))
 
+# Clean name of Crude Rate to suicideratetotal
+colnames(suicides_data)[6] <- "suicideratetotal"
 
+# Store Data as cleaned_suicides
+cleaned_suicides <- suicides_data
+
+## Clean Abortion Rates Data for Abortion Rates betweene years 1973-2006. 
+
+# Open CSV and save into abortion_data
+abortion_data <- read_csv("inputs/data/NationalAndStatePregnancy_PublicUse.csv")
+
+# Select only data in the whole US Country, excluding states.
+abortion_data <- filter(abortion_data, state %in% c("US"))
+
+# Select only data in the years 1973 to 2016
+#abortion_data <- filter(abortion_data, year %in% c(1973:2016))
+
+# Keep columns only in years, abortion rate total, and abortion ratio total in each year. 
+# I included both abortion rate total and abortion ratio, I included abortion ratio as 
+# I believe it's a better representation of the increase in the use of abortion services after legalization.
+# But the rate is better example for our use case here
+abortion_data <- subset(abortion_data, select = c(year,abortionstotal, pregnanciestotal, population1544, abortionratetotal, abortionratiototal))
+
+# Show data for double checking.
+abortion_data
+
+# Keep cleaned data in new frame with new name.
+cleaned_abort <- abortion_data
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change cleaned_data to whatever name you end up with at the end of cleaning
-write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
+# Save files as cleaned_abort to clean-abort, and cleaned_suicides.
+write_csv(cleaned_abort, "outputs/clean_data/cleaned_abort.csv")
+write_csv(cleaned_suicides, "outputs/clean_data/cleaned_suicides.csv")
 
